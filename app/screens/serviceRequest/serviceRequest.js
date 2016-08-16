69b73098b4a24e24263706dd123efaca8145191c
sjones <jonesin@gmail.com>
2016-08-16 17:32:06 -0500

    Integrated saved services

--- app/screens/serviceRequest/serviceRequest.js
+++ app/screens/serviceRequest/serviceRequest.js
@@ -18,10 +18,65 @@ import Spinner from 'react-native-loading-spinner-overlay';
 
 var width = Dimensions.get('window').width - 20;
 
+var MAINTENANCE_URL = 'http://ec2-52-34-200-111.us-west-2.compute.amazonaws.com:3001/api/v1/vehicles/?/services';
+
 class ServiceRequest extends Component {
 
+constructor(props) {
+  super(props);
+  this.state = {
+    services:null,
+    total:0,
+    visible: false,
+  };
+}
+
+componentDidMount() {
+  this.getMaintenance();
+}
+
+getMaintenance() {
+  if(this.props.isLoggedIn && this.props.vehicleId)
+  {
+    fetch(MAINTENANCE_URL.replace("?", this.props.vehicleId), {headers: {'Authorization': this.props.authentication_token}})
+      .then((response) => response.json())
+      .then((responseData) => {
+        var total = 0;
+        this.setState({
+          services: responseData.vehicles,
+          total: "$" + total.toFixed(2)
+        });
+      })
+      .done();
+  }
+}
+
 render() {
+  if (!this.state.services) {
+    return this.renderLoadingView();
+  }
+  var services = this.state.services;
+  var nav = this.props.navigator;
+  return this.renderServices(services,nav);
+}
+
+renderLoadingView() {
+  return (
+    <View>
+      <Spinner visible={true} />
+    </View>
+  );
+}
+
+filterSavedServices(service)
+{
+  return service.status == 2;
+}
+
+renderServices(services,nav) {
+    var savedServices = services.filter(this.filterSavedServices.bind(this));
     return (
+
       <View style={styles.base}>
         <TopBar navigator={this.props.navigator} />
         <CarBar />
@@ -79,19 +134,7 @@ render() {
           </View>
 
           <Text style={styles.textHd}>Saved Services</Text>
-          <View style={styles.serviceRow}>
-            <Text style={styles.serviceItem}>Oil Change - Synthetic Blend</Text>
-            <View style={styles.fairPriceContainer}>
-              <Text style={styles.fairPriceText}>FAIR PRICE</Text>
-              <View style={styles.fairPriceRange}>
-                <Text style={styles.fairPrice}>$30</Text>
-                <Image
-                  source={require('../../../images/arrow-range.png')}
-                  style={styles.fairPriceArrow} />
-                <Text style={styles.fairPrice}>$50</Text>
-              </View>
-            </View>
-          </View>
+          {savedServices.map(createServiceRow)}
 
           <View style={styles.rowAddService}>
             <TouchableOpacity onPress={() => this.props.navigator.push({ indent:'AddServices' })}>
@@ -104,9 +147,39 @@ render() {
         </View>
         </ScrollView>
       </View>
+
     );
-  }
 }
+}
+
+var createServiceRow = (service, i) => <Service key={i} service={service} />;
+
+var Service = React.createClass({
+  shouldComponentUpdate: function(nextProps, nextState) {
+  return false;
+},
+render: function() {
+return (
+  <TouchableOpacity
+    onPress={() => this.props.navigator.push({ indent:'ServiceRequestDetail' })}>
+    <View style={styles.serviceRow}>
+      <Text style={styles.serviceItem}>{this.props.service.name}</Text>
+      <View style={styles.fairPriceContainer}>
+        <Text style={styles.fairPriceText}>FAIR PRICE</Text>
+        <View style={styles.fairPriceRange}>
+          <Text style={styles.fairPrice}>${Number(this.props.service.low_fair_cost).toFixed(0)}</Text>
+          <Image
+            source={require('../../../images/arrow-range.png')}
+            style={styles.fairPriceArrow} />
+          <Text style={styles.fairPrice}>${Number(this.props.service.high_fair_cost).toFixed(0)}</Text>
+        </View>
+      </View>
+    </View>
+  </TouchableOpacity>
+);
+}
+});
+
 
 var styles = StyleSheet.create({
   base: {
